565bdde8e27947ac57f088fb04873fb436be4978
Marisol <00094716@uca.edu.sv>
2022-03-05 23:03:16 -0600

    Requerir paquetes

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+const fs = require('fs');
+const num = 3;
 
 console.clear();
 console.log('=======================');
-console.log('      Tabla del 5      ');
+console.log(`      Tabla del ${num}      `);
 console.log('=======================');
 
-const num = 5;
+let salida = '';
 
 for(let i = 1; i <= 10; i++){
-    console.log(`${num} x ${i} = ${num*i}`);
+    //console.log(`${num} x ${i} = ${num*i}`);
+    salida += `${num} x ${i} = ${num*i}\n`;
 }
+
+console.log(salida);
+
+fs.writeFile(`tabla-${num}.txt`, salida, (err) => {
+    if(err) throw err;
+    console.log(`Tabla-${num}.txt`)
+})
